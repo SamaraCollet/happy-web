aff627b7af3297189600069f1ce230a923c96d8b
SamaraCollet <61025305+SamaraCollet@users.noreply.github.com>
2022-08-11 14:52:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles/global.css";
+import "leaflet/dist/leaflet.css"; // estilização padrão do leaflet
+
+import Routes from "./routes";
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
